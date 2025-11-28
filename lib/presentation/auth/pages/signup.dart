7df1e918efeb71a69c62b1bd01edb2a14ffa1f0d
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-28 13:33:24 +0700

    Mengimplementasikan auth desain untuk bagian sign up    - Ini masih dalam bentukan UI

--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -1,10 +1,76 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app/common/helper/navigation/app_navigation.dart';
+import 'package:movie_app/core/configs/theme/app_colors.dart';
+import 'package:movie_app/presentation/auth/pages/signin.dart';
+import 'package:reactive_button/reactive_button.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        minimum: EdgeInsets.only(top: 100, right: 16, left: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _signupText(),
+            SizedBox(height: 30),
+            _emailField(),
+            SizedBox(height: 20),
+            _passwordField(),
+            SizedBox(height: 60),
+            _signupButton(),
+            SizedBox(height: 10),
+            _signinText(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signupText() {
+    return Text(
+      'Sign Up',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    );
+  }
+
+  Widget _emailField() {
+    return TextField(decoration: InputDecoration(hintText: 'Email'));
+  }
+
+  Widget _passwordField() {
+    return TextField(decoration: InputDecoration(hintText: 'Password'));
+  }
+
+  Widget _signupButton() {
+    return ReactiveButton(
+      title: 'Sign Up',
+      activeColor: AppColors.primary,
+      onPressed: () async {},
+      onSuccess: () {},
+      onFailure: (error) {},
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(text: 'Have an account?'),
+          TextSpan(
+            text: ' Sign In',
+            style: TextStyle(color: Colors.blue),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                AppNavigator.push(context, SigninPage());
+              },
+          ),
+        ],
+      ),
+    );
   }
 }
